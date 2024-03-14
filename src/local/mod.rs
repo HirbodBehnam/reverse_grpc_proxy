34b6957c3033a87c1c3c9c4162dfa2b16e66b5c6
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2024-03-14 21:54:53 +0330

    Fixed shared state

--- src/local/mod.rs
+++ src/local/mod.rs
@@ -1,4 +1,4 @@
-use std::os::unix::net::SocketAddr;
+use std::{borrow::Borrow, os::unix::net::SocketAddr, sync::Arc};
 
 use log::info;
 
@@ -8,22 +8,25 @@ mod socket;
 pub async fn start_local_server(cf_listen_address: &str, local_listen_address: &str) {
     // Create shared states.
     // We can simply leak these values to do not pay for reference counting because we need them for the rest of the program.
-    let reverse_proxy_local: &'static grpc::ReverseProxyLocal =
-        Box::leak(Box::new(grpc::ReverseProxyLocal::default()));
+    let reverse_proxy_local = Arc::new(grpc::ReverseProxyLocal::default());
 
     // Tell tonic to run the app
+    let cf_listen_socket = cf_listen_address
+        .parse()
+        .expect("cannot parse the socket address");
+    info!("Cloudflare listen is {cf_listen_address}");
+    let reverse_proxy_local_service = grpc::proxy::proxy_controller_server::ProxyControllerServer::from_arc(
+        Arc::clone(&reverse_proxy_local),
+    );
     tokio::spawn(async move {
-        let cf_listen_socket = cf_listen_address
-            .parse()
-            .expect("cannot parse the socket address");
-        info!("Cloudflare listen is {cf_listen_address}");
         tonic::transport::Server::builder()
-            .add_service(grpc::ReverseProxyLocal::default())
+            .add_service(reverse_proxy_local_service)
             .serve(cf_listen_socket)
-            .await;
+            .await
+            .expect("cannot listen for cloudflare");
     });
 
     // In main thread, wait for TCP sockets
     info!("Local listen is {local_listen_address}");
-    socket::handle_socket(&local_listen_address, reverse_proxy_local).await;
+    socket::handle_socket(&local_listen_address, reverse_proxy_local.borrow()).await;
 }
