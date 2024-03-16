edc1c3de73a181250c8d5c7d489c259d26789f86
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2024-03-16 20:01:00 +0330

    Wrote the main file Now I should test this shit...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use clap::Parser;
+
+mod arguments;
 mod local;
 mod remote;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let args = arguments::Args::parse();
+    // Start the server or client
+    match args.command {
+        arguments::Commands::Local {
+            tcp_listen_address,
+            cloudflare_listen_address,
+        } => local::start_local_server(&cloudflare_listen_address, &tcp_listen_address).await,
+        arguments::Commands::Server {
+            cloudflare_server_address,
+            forward_address,
+        } => remote::start_remote_server(cloudflare_server_address, &forward_address).await,
+    };
 }
